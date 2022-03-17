f7a4f08869bd23c974c6d61be54c298395c3f29d
ntluan95 <ntluandt95@gmail.com>
2022-03-17 13:47:47 -0500

    deploy

--- src/main/java/com/example/AzureDemo/controllers/HelloController.java
+++ src/main/java/com/example/AzureDemo/controllers/HelloController.java
@@ -1,19 +1,24 @@
 package com.example.AzureDemo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.AzureDemo.Student;
+import com.example.AzureDemo.studentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello from Spring Boot!";
+    @Autowired
+    studentRepo repo;
+
+    @GetMapping("/{id}")
+    public Student hello(@PathVariable int id) {
+        return repo.findById(id).orElse(null);
     }
 
-    @GetMapping("/fruit")
-    public String fruit() {
-        return "apples";
+    @PostMapping("/")
+    public Student fruit(@RequestBody Student student) {
+        return repo.save(student);
     }
 }
 
---
+++ src/main/java/com/example/AzureDemo/studentRepo.java
@@ -0,0 +1,7 @@
+package com.example.AzureDemo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface studentRepo extends CrudRepository<Student,Integer> {
+
+}
