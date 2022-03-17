4644405983e919ed95073bce232f20df9ba4ec06
ntluan95 <ntluandt95@gmail.com>
2022-03-17 13:56:12 -0500

    deploy

--- src/main/java/com/example/AzureDemo/controllers/HelloController.java
+++ src/main/java/com/example/AzureDemo/controllers/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloController {
 
     @Autowired
